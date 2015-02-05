5a88af1206a2e86aa20ea6ef1668e5b2027d8e54
Yair Sladowsky <ye.sladowsky@gmail.com>
2015-02-05 14:53:30 +0200

    Removed PHP shorttags

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -22,15 +22,15 @@
 
 	<p><label for="ipv_script">Please enter the embed code provided by your iProfitVideo Application here.</label></p>
 
-	<textarea name="ipv_script" id="jscode" class="large-text code" style="width:676px;height:350px"><?php echo get_option( 'ipv_script' );?></textarea>
+	<textarea name="ipv_script" id="jscode" class="large-text code" style="width:676px;height:350px"><?= get_option( 'ipv_script' );?></textarea>
 	
 	<p>By Default, the widget is embedded on all pages.</p>
 	
-	<p><label><input type="radio" id="radio_on" name="pages_radio"<? echo (($on_pages = get_option( 'ipv_on_pages' ))?' checked':null) ?>> To enable the widget on specific pages only, enter the page titles below, one per line:</label></p>
-	<textarea id="on_pages" name="ipv_on_pages" style="height:150px;resize:none;" class="regular-text"<? echo ($on_pages?null:' disabled') ?>><?php echo $on_pages ?></textarea>
+	<p><label><input type="radio" id="radio_on" name="pages_radio"<?= (($on_pages = get_option( 'ipv_on_pages' ))?' checked':null) ?>> To enable the widget on specific pages only, enter the page titles below, one per line:</label></p>
+	<textarea id="on_pages" name="ipv_on_pages" style="height:150px;resize:none;" class="regular-text"<?= ($on_pages?null:' disabled') ?>><?= $on_pages ?></textarea>
 	
-	<p><label><input type="radio" id="radio_off" name="pages_radio"<? echo (($off_pages = get_option( 'ipv_off_pages' ))?' checked':null) ?>> To exclude the widget from specific pages (but display on all others), enter the page titles below, one per line:</label></p>
-	<textarea id="off_pages" name="ipv_off_pages" style="height:150px;resize:none;" class="regular-text"<? echo ($off_pages?null:' disabled') ?>><?php echo $off_pages ?></textarea>
+	<p><label><input type="radio" id="radio_off" name="pages_radio"<?= (($off_pages = get_option( 'ipv_off_pages' ))?' checked':null) ?>> To exclude the widget from specific pages (but display on all others), enter the page titles below, one per line:</label></p>
+	<textarea id="off_pages" name="ipv_off_pages" style="height:150px;resize:none;" class="regular-text"<?= ($off_pages?null:' disabled') ?>><?= $off_pages ?></textarea>
 	
 	<br><br><span class="description">
 	Note: Do not include the file extension (like .php), quotation marks, or any other punctuation,<br>
